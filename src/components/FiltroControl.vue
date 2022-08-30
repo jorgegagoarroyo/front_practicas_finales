5f23a329cd412387ef17b99cf95ef72c2ba05f79
jorgegagoarroyo <gagoarroyojorge@fpllefia.com>
2022-08-31 00:41:11 +0200

    cambios varios pre router

--- src/components/FiltroControl.vue
+++ src/components/FiltroControl.vue
@@ -1,23 +1,69 @@
 <template>
-  
+<div class="container-fluid">
+    <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <div class="row">
+          <div class="col">CURSO:</div>
+          <select class="col" name="cursos" v-if="cursos" @change="sele_true('cursos')" v-model="val_cursos" >
+            <option v-for="(opt, index) in cursos" :value="opt" :key="index">{{opt.nombre}}</option>
+          </select>
+        </div>
+      </div>
+      <div v-if="s_cursos" class="col-4">
+        <div class="row">
+          <template v-if="clases">
+            <div class="col">CLASE:</div>
+            <select class="col" name="clases" @change="sele_true('clases')" v-model="val_clases" >
+              <option v-for="(opt, index) in clases" :value="opt" :key="index" >{{opt.nombre}}</option>
+            </select>
+          </template>
+      </div>
+      </div>
+      <div v-if="s_clase" class="col-4">
+        <div class="row">
+          <div class="col">TUTOR</div>
+          <select class="col" name="tutores" @change="sele_true('tutor')" v-model="val_tutor" v-if="clases">
+            <option v-for="(opt, index) in tutores" :value="opt" :key="index" :title="opt.nombre +' '+opt.apellido1">{{opt.usuario}}</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import PanelControl from './PanelControl.vue'
+// ----------------------------------------------------------------------------------------
+// falta agregar la llamada al panel y midificat codigos empleados
+// ----------------------------------------------------------------------------------------
+// import PanelControl from './PanelControl.vue'
 export default {
   name: 'FiltroControl',
-  components: {
-    PanelControl
+  // components: {
+  //   // PanelControl
+  // },
+  data () {
+    return {
+      s_cursos: false,
+      s_clase: false,
+      s_tutor: false,
+      cursos: false,
+      clases: false,
+      tutores: false,
+      val_cursos: '',
+      val_clases: '',
+      val_tutor: '',
+      token: ''
+    }
   },
   methods: {
     async get_alumnos () {
-      let token = localStorage.getItem('control')
-      token = JSON.parse(token)
       const uri = 'http://localhost:4000/api/clase_alumno/get'
       let respon = await fetch(uri, {
         method: 'POST',
         headers: {
-          authorization: `bearer ${token.token}`,
+          authorization: `bearer ${this.token.token}`,
           'Content-type': 'application/json; charset=UTF-8'
         },
         body: JSON.stringify({
@@ -29,10 +75,116 @@ export default {
       respon = respon.json()
       // respon = respon[0].resul
       this.datos = respon
-      console.log(uri)
-      console.log('res ', this.datos)
-      this.listado = true
+    },
+    sele_true (x) {
+      if (x === 'cursos') {
+        this.s_cursos = false
+        this.get_clases()
+        this.s_cursos = true
+        console.log('curso cambio')
+      }
+      if (x === 'clases') {
+        this.s_clase = true
+        this.get_tutor()
+      }
+      if (x === 'cursos') {
+        this.s_tutor = true
+      }
+    },
+    async get_cursos () {
+      const uri = 'http://localhost:4000/api/cursos/get'
+      let respon = await fetch(uri, {
+        method: 'POST',
+        headers: {
+          authorization: `bearer ${this.token.token}`,
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      })
+      // console.log(respon)
+      respon = await respon.json()
+      this.cursos = respon.resul
+      // console.log(this.cursos)
+    },
+    async get_clases () {
+      this.clases = false
+      const uri = 'http://localhost:4000/api/clases/get'
+      let respon = await fetch(uri, {
+        method: 'POST',
+        headers: {
+          authorization: `bearer ${this.token.token}`,
+          'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+          campos: { // agregar otros filtros
+            id_cursos: this.val_cursos.id
+          }
+        })
+      })
+      respon = await respon.json()
+      this.clases = respon.resul
+      // console.log(this.val_cursos.id)
+      // console.log(respon.resul)
+      // console.log(this.cursos)
+    },
+    async get_tutor () {
+      const uri = 'http://localhost:4000/api/clases_curso/get'
+      let respon = await fetch(uri, {
+        method: 'POST',
+        headers: {
+          authorization: `bearer ${this.token.token}`,
+          'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+          campos: { // agregar otros filtros
+            id_cursos: this.val_cursos.id,
+            id_clases: this.val_clases.id
+          }
+        })
+      })
+      respon = await respon.json()
+      // this.tutores = respon.resul
+      this.get_empleado(respon.resul)
+      // console.log('curso ', this.val_cursos.id)
+      // console.log('clase ', this.val_clases.id)
+      // console.log(this.tutores)
+      // console.log(respon.resul)
+      // console.log(this.cursos)
+    },
+    async get_empleado (data) {
+      console.log('data ', data)
+      const uri = 'http://localhost:4000/api/usuarios/get'
+      let respon = await fetch(uri, {
+        method: 'POST',
+        headers: {
+          authorization: `bearer ${this.token.token}`,
+          'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+          campos: { // agregar otros filtros
+            id: data[0].id_empleados,
+            id_roles: 1 // admin 1 para pruebas y 3 para profesores
+          }
+        })
+      })
+      respon = await respon.json()
+      this.tutores = respon.resul
+      console.log(respon)
+    },
+    get_token () {
+      let token = localStorage.getItem('control')
+      token = JSON.parse(token)
+      this.token = token
     }
+  },
+  computed: {
+    c_clase: () => {
+      return this.clases
+    },
+    c_tutor: () => {}
+  },
+  mounted () {
+    this.get_token()
+    this.get_cursos()
   }
 }
 </script>
--- src/components/MultiTest.vue
+++ src/components/MultiTest.vue
@@ -3,20 +3,23 @@
   <!-- <casilla-asistencia class="col border" :alumno="1" :horario="2" :valor="lista.a" :valores="lista" />
   <casilla-asistencia class="col" :alumno="1" :horario="3" :valor="lista.b" :valores="lista" />
   <casilla-asistencia class="col" :alumno="1" :horario="4" :valor="lista.d" :valores="lista" /> -->
-  <panel-control/>
+  <!-- <panel-control/> -->
   <!-- <info-panel/> -->
+  <filtro-control/>
 </div>
 </template>
 
 <script>
 // import CasillaAsistencia from './CasillaAsistencia.vue'
-import PanelControl from './PanelControl.vue'
+// import PanelControl from './PanelControl.vue'
 // import InfoPanel from './InfoPanel.vue'
+import FiltroControl from './FiltroControl.vue'
 export default {
   name: 'MultiTest',
   components: {
+    FiltroControl
     // CasillaAsistencia
-    PanelControl
+    // PanelControl
     // InfoPanel
   },
   data () {
